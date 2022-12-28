3d320fd07a78a987197db8837ffc162881836a22
Gabriel Dias <gabriel_dias_santiago@hotmail.com>
2022-12-28 17:12:35 -0300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,6 @@ import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.scss'
 import Page from './Page'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <>
